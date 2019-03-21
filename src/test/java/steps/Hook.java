fbc50be0abfbd6eb1976a9c7fbce97ccae649fa8
Celestino da Cruz Cortez <celestinocortez@gmail.com>
2019-03-21 15:10:17 -0300

    updated chromedriver path

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,8 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/celestino/Ambiente/chromedriver/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
